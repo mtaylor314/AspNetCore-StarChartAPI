d0d6d753eae4f1a50d18e91acf1a735e8d6d2b75
Michael Taylor <michael@taylorwebsolutions.com>
2022-08-24 14:19:00 -0400

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -12,6 +13,39 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var co = _context.CelestialObjects.FirstOrDefault(c => c.Id == id);
+
+            if (null == co) return NotFound();
+
+            co.Satellites = co.Satellites.Where(c => c.OrbitedObjectId == id).ToList();
+
+            return Ok(co);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var co = _context.CelestialObjects.FirstOrDefault(c => c.Name == name);
+
+            if (null == co) return NotFound();
+
+            co.Satellites = co.Satellites.Where(c => c.OrbitedObjectId == co.Id).ToList();
+
+            return Ok(co);
+        }
+
+        public IActionResult GetAll()
+        {
+            var co = _context.CelestialObjects.ToList();
+
+
+
+            return Ok(co);
         }
     }
 }
